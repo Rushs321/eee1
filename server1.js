eccc43a6237b9b7b8fd47cfcbf7ddfca8d78a8e5
Rushs321 <fyggh566@tutamail.com>
2024-08-24 23:46:53 +0530

    Update server1.js

--- server1.js
+++ server1.js
@@ -1,3 +1,25 @@
+#!/usr/bin/env node
+'use strict'
+const cluster = require("cluster");
+const os = require("os");
+
+if (cluster.isPrimary) {
+  const numClusters = process.env.CLUSTERS || (os.availableParallelism ? os.availableParallelism() : (os.cpus().length || 2))
+
+  console.log(`Primary ${process.pid} is running. Will fork ${numClusters} clusters.`);
+
+  // Fork workers.
+  for (let i = 0; i < numClusters; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died. Forking another one....`);
+    cluster.fork();
+  });
+
+  return true;
+}
 const fastify = require('fastify')();
 const express = require('@fastify/express');
 const proxy = require('./src/proxy');
