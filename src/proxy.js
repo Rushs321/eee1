aae4da5755c9451a149b2bbca1aa593a1a7d81cf
Rushs321 <fyggh566@tutamail.com>
2024-08-24 23:07:01 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -28,7 +28,7 @@ async function proxy(req, res) {
 
   const DEFAULT_QUALITY = 40
 
-function params(req, res, next) {
+
   let url = req.query.url;
   if (!url) return res.send('bandwidth-hero-proxy');
 
@@ -36,9 +36,6 @@ function params(req, res, next) {
   req.params.webp = !req.query.jpeg
   req.params.grayscale = req.query.bw != 0
   req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY
-
-  next()
-}
 
     const randomIP = generateRandomIP();
     const userAgent = randomUserAgent();
