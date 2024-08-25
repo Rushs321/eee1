ea27c7e818f4b7372c3016e2e9e147fe27614508
Rushs321 <fyggh566@tutamail.com>
2024-08-25 10:01:54 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -1,9 +1,4 @@
 "use strict";
-/*
- * proxy.js
- * The bandwidth hero proxy handler.
- * proxy(httpRequest, httpResponse);
- */
 const undici = require("undici");
 const pick = require("lodash").pick;
 const { generateRandomIP, randomUserAgent } = require('./utils');
@@ -24,97 +19,87 @@ function randomVia() {
     return viaHeaders[index];
 }
 
-async function proxy(req, res) {
+async function proxy(req, reply) {
 
-  const DEFAULT_QUALITY = 40
-
+  const DEFAULT_QUALITY = 40;
 
   let url = req.query.url;
-  if (!url) return res.send('1we23');
+  if (!url){ 
+      const ipAddress = generateRandomIP();
+      const ua = randomUserAgent();
+      const hdrs = {
+          ...pick(req.headers, ["cookie", "dnt", "referer", "range"]),
+          'user-agent': ua,
+          'x-forwarded-for': ipAddress,
+          'via': randomVia(),
+      };
+          
+      Object.entries(hdrs).forEach(([key, value]) => reply.header(key, value));
+      
+      return reply.send('1we23');
+  }
 
   req.params.url = decodeURIComponent(url);
-  req.params.webp = !req.query.jpeg
-  req.params.grayscale = req.query.bw != 0
-  req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY
+  req.params.webp = !req.query.jpeg;
+  req.params.grayscale = req.query.bw != 0;
+  req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY;
 
-    const randomIP = generateRandomIP();
-    const userAgent = randomUserAgent();
-  
-  /*
-   * Avoid loopback that could causing server hang.
-   */
- /* if (
-    req.headers["via"] == "1.1 bandwidth-hero" &&
-    ["127.0.0.1", "::1"].includes(req.headers["x-forwarded-for"] || req.ip)
-  )
-    return redirect(req, res);*/
+  const randomIP = generateRandomIP();
+  const userAgent = randomUserAgent();
+
   try {
     let origin = await undici.request(req.params.url, {
       headers: {
         ...pick(req.headers, ["cookie", "dnt", "referer", "range"]),
         'user-agent': userAgent,
-                'x-forwarded-for': randomIP,
-                'via': randomVia(),
+        'x-forwarded-for': randomIP,
+        'via': randomVia(),
       },
       timeout: 10000,
       maxRedirections: 4
     });
-    _onRequestResponse(origin, req, res);
+    _onRequestResponse(origin, req, reply);
   } catch (err) {
-    _onRequestError(req, res, err);
+    _onRequestError(req, reply, err);
   }
 }
 
-function _onRequestError(req, res, err) {
-  // Ignore invalid URL.
-  if (err.code === "ERR_INVALID_URL") return res.status(400).send("Invalid URL");
+function _onRequestError(req, reply, err) {
+  if (err.code === "ERR_INVALID_URL") return reply.status(400).send("Invalid URL");
 
-  /*
-   * When there's a real error, Redirect then destroy the stream immediately.
-   */
-  redirect(req, res);
+  redirect(req, reply);
   console.error(err);
 }
 
-function _onRequestResponse(origin, req, res) {
+function _onRequestResponse(origin, req, reply) {
   if (origin.statusCode >= 400)
-    return redirect(req, res);
+    return redirect(req, reply);
 
-  // handle redirects
   if (origin.statusCode >= 300 && origin.headers.location)
-    return redirect(req, res);
+    return redirect(req, reply);
 
-  copyHeaders(origin, res);
-  res.setHeader("content-encoding", "identity");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
-  res.setHeader("Cross-Origin-Embedder-Policy", "unsafe-none");
+  copyHeaders(origin, reply);
+  reply.header("content-encoding", "identity");
+  reply.header("Access-Control-Allow-Origin", "*");
+  reply.header("Cross-Origin-Resource-Policy", "cross-origin");
+  reply.header("Cross-Origin-Embedder-Policy", "unsafe-none");
   req.params.originType = origin.headers["content-type"] || "";
   req.params.originSize = origin.headers["content-length"] || "0";
 
   origin.body.on('error', _ => req.socket.destroy());
 
   if (shouldCompress(req)) {
-    /*
-     * sharp support stream. So pipe it.
-     */
-    return compress(req, res, origin);
+    return compress(req, reply, origin);
   } else {
-    /*
-     * Downloading then uploading the buffer to the client is not a good idea though,
-     * It would better if you pipe the incomming buffer to client directly.
-     */
-
-    res.setHeader("x-proxy-bypass", 1);
+    reply.header("x-proxy-bypass", 1);
 
     for (const headerName of ["accept-ranges", "content-type", "content-length", "content-range"]) {
       if (headerName in origin.headers)
-        res.setHeader(headerName, origin.headers[headerName]);
+        reply.header(headerName, origin.headers[headerName]);
     }
 
-    return origin.body.pipe(res);
+    return origin.body.pipe(reply.raw);
   }
 }
-
 
 module.exports = proxy;
